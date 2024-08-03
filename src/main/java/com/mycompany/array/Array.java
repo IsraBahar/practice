bac5f34e6cadb52f5cce476dc529dd6a5bd06173
IsraBahar <baharis@sheridancollege.ca>
2024-08-02 22:50:49 -0400

    my name is Isra

---
+++ src/main/java/com/mycompany/array/Array.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.array;
+
+/**
+ *
+ * @author issra
+ */
+public class Array {
+
+    public static void main(String[] args) {
+        
+        String[][] Numbers = { {"1996 " , "1997", "1998 ", "1999 ", "2000 "},
+                               {"1996 ", "1997 ", "1998 ", "1999 ", "2000 "},
+                               {"1996 ", "1997 ", "1998 ", "1999 ", "2000 "},
+        };
+        for(int i=0; i<Numbers.length; i++){
+            System.out.println();
+        for(int j=0; j<Numbers[i].length;j++){
+            System.out.print(Numbers[i][j] +"");
+    }
+        }
+    
+}
+} 
